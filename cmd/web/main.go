047970bff74910535014be2738415e3b7ab3afcf
GipsonL <78498165+GipsonL@users.noreply.github.com>
2025-07-02 23:49:48 -0500

    chapter 3.3

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,26 +2,35 @@ package main
 
 import (
 	"flag"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
-
 	addr := flag.String("addr", ":4000", "HTTP network address")
-
 	flag.Parse()
-
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	// Initialize a new instance of our application struct, containing the
+	// dependencies (for now, just the structured logger).
+	app := &application{
+		logger: logger,
+	}
 	mux := http.NewServeMux()
-
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
-	log.Printf("starting server on %s", *addr)
-
+	// Swap the route declarations to use the application struct's methods as the
+	// handler functions.
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+	logger.Info("starting server", "addr", *addr)
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
