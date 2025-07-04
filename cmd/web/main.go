5afb3c99cd62fce9aa8e9fb628c2563537599beb
GipsonL <78498165+GipsonL@users.noreply.github.com>
2025-07-04 15:43:15 -0500

    chpt 3.5

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -20,17 +20,9 @@ func main() {
 	app := &application{
 		logger: logger,
 	}
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-	// Swap the route declarations to use the application struct's methods as the
-	// handler functions.
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+
 	logger.Info("starting server", "addr", *addr)
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
